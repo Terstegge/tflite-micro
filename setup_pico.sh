534f1ac19cbb1932991e86e360541451c19d57d2
thew0205 <busoyetm@gmail.com>
2025-08-31 13:10:01 +0100

    Remove the kissfft as it is only needed for the signal folder.

--- setup_pico.sh
+++ setup_pico.sh
@@ -7,8 +7,6 @@ git clone --depth 1 https://github.com/google/flatbuffers.git --branch v23.5.26 
 
 git clone --depth 1 https://github.com/google/ruy.git ./third_party/ruy
 
-git clone --depth 1 https://github.com/mborgerding/kissfft.git --branch 131.1.0 ./third_party/kissfft
-
 git clone --depth 1 https://github.com/ARM-software/CMSIS-NN.git --branch v7.0.0 ./third_party/CMSIS-NN
 
 git clone --depth 1 https://github.com/ARM-software/CMSIS_5.git ./third_party/CMSIS_5
